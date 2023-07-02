e49a5149762b2d93c4335829cceca1f9842be953
Mendy2050 <107521996+Mendy2050@users.noreply.github.com>
2023-07-02 16:00:00 +0100

    Add files via upload

---
+++ springboot-01-Quick_Start/src/main/java/com/itheima/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mendy
+ * @create 2023-07-02 11:16
+ */
+
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id){
+        System.out.println("id==>" + id);
+        return "hello, spring boot";
+    }
+
+}
